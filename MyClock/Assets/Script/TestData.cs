9ad26e5077940dd35b2350fb823fa8822b40ee58
EDIANZU-5KETN43\Administrator <3243701143@qq.com>
2018-10-08 17:15:55 +0800

    测试

---
+++ MyClock/Assets/Script/TestData.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using calendar;
+
+public class TestData : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+
+        //ChineseCalendar cc = new ChineseCalendar(System.DateTime.Now);
+        
+        ChineseCalendar cc = new ChineseCalendar(Convert.ToDateTime("2018/12/5"));
+        Debug.Log(cc.ChineseDateString);
+        Debug.Log(cc.ChineseTwentyFourDay);
+        Debug.Log(cc.AnimalString);
+        Debug.Log(cc.Constellation);
+        Debug.Log(cc.WeekDayStr);
+        Debug.Log(cc.GanZhiDateString);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
